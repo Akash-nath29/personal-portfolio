e652fe4992edc67fac0ed3408705ef65a910812f
Akash-nath29 <anath5440@gmail.com>
2024-03-26 21:55:00 +0530

    Hero Section Update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import './css/Hero.css'
+import photo from './assets/photo.jpg'
 
-const Hero = () => {
+export default function Hero() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="heroBg w-[100vw] h-[100vh] py-24 flex items-center justify-evenly">
+      <div className="leftContent">
+        <h1 className="text-5xl font-bold">Hi, I am Akash <span id='helloIcon'>👋</span></h1>
+        <p className="mt-4 text-lg">Fullstack developer, AI-ML enthusiast, Engineering Student</p>
+      </div>
+      <div className="rightContent">
+        <img src={photo} alt="" className='img w-[500px] rounded'/>
+      </div>
     </div>
   )
 }
-
-export default Hero
